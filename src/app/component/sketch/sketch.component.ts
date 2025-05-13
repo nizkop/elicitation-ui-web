69fc780ce12bd259e44428340f720646b643b566
Judith <judith.leson@stud.uni-due.de>
2025-05-13 19:43:45 +0200

    1.05*x, 1*y

--- src/app/component/sketch/sketch.component.ts
+++ src/app/component/sketch/sketch.component.ts
@@ -597,17 +597,19 @@ private formatTimestamp(timestamp: number): string {
                     for (const line of sheetDrawings) {
                         if (line.length > 1) {
                             tempContext.beginPath();
-                            const offset_x_for_tablet = 1.0;
+                            const offset_x_for_tablet = 1.05;
                             // 1.15 zu weit rechts#
                             // 0.75 zu weit links
-                            const offset_y_for_tablet = 1.5;
+                            // 1 zu links
+                            const offset_y_for_tablet = 1.0;
                             // 0.75 zu schmal & zu hoch
                             // 0.5 zu hoch
+                            // 1.5 zu tief
 
                             // Transform the first point
                             const startX: number = line[0].x * scaleX*offset_x_for_tablet;// TODO : const startX: number = line[0].x //* scaleX;
                             const startY: number = line[0].y * scaleY*offset_y_for_tablet;// TODO : const startY: number = line[0].y //* scaleY;
-                            alert(`*1.15 (x) -> startX: ${startX}, *0.75 (y) startY: ${startY}`);
+                            alert(`*1.05 (x) -> startX: ${startX}, *1 (y) startY: ${startY}`);
                             // Tablet: 539.194..., 74.771... = nach o
                             // +10 = nach s
                             // + 100 (y) -> 170 = nach o

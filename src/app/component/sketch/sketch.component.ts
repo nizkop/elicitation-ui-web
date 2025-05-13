93e7b54e55fce9688a526c79d887849c9d0a4aac
Judith <judith.leson@stud.uni-due.de>
2025-05-13 20:19:23 +0200

    1.095*x, 1.095*y

--- src/app/component/sketch/sketch.component.ts
+++ src/app/component/sketch/sketch.component.ts
@@ -597,7 +597,7 @@ private formatTimestamp(timestamp: number): string {
                     for (const line of sheetDrawings) {
                         if (line.length > 1) {
                             tempContext.beginPath();
-                            const offset_x_for_tablet = 1.09;
+                            const offset_x_for_tablet = 1.095;
                             // 1.15 zu weit rechts#
                             // 0.75 zu weit links
                             // 1 zu links
@@ -606,7 +606,8 @@ private formatTimestamp(timestamp: number): string {
                             // 1.08 zu weit rechts
                             // 1.07 zu weit links!
                             // 1.079 zu weit links!
-                            const offset_y_for_tablet = 1.09;
+                            // 1.09 zu weit links
+                            const offset_y_for_tablet = 1.095;
                             // 0.75 zu schmal & zu hoch
                             // 0.5 zu hoch
                             // 1.5 zu tief
